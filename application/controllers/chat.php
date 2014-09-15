b678943d30ab88d42f6f8689b17e933257a7b31c
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2014-09-15 10:42:52 -0700

    Creating the controller for the SMChat application

---
+++ application/controllers/chat.php
@@ -0,0 +1,19 @@
+<?php 
+
+class Chat extends CI_Controller {
+
+	public function chat() 
+	{
+	
+	
+	}
+	
+	public function index()
+	{
+		
+		$this->load->view('chat_view');
+		
+	
+	}
+
+}
---
+++ application/views/chat_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> A Chat Application</title>
+<script 
+</head>
+<body>
+<h1>This is a Simple Chat Application</h1>
+
+<?php
+
+?>
+
+</body>
+</html>
+	
